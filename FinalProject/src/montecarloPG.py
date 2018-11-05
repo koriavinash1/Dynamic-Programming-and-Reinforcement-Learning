[FIX] montecarlo policy gradient

FinalProject/src/montecarloPG.py
@@ -4,45 +4,61 @@ import gym
 import os
 from gym import wrappers
 
+## TRAINING Hyperparameters
+NUM_EPISODES = 500
+MAX_T = 10000
+GAMMA = 0.99 # Discount rate
+LEARNING_RATE = 0.001
+EXPLORATION_RATE = 1.0
+EXPLORATION_RATE_DECAY = 0.010
+MIN_EXPLORATION_RATE = 0.1
+LAMBDA = 0.01
 
 
 OUTPUT_RESULTS_DIR = "../logs"
-ENVIRONMENT = 'InvertedDoublePendulum-v2'
+ENVIRONMENT = 'CartPole-v1'
 
 # TIMESTAMP = datetime.now().strftime("%Y%m%d-%H%M%S")
 TIMESTAMP = 'RESULTS'
 SUMMARY_DIR = os.path.join(OUTPUT_RESULTS_DIR, "MonteCarloPG", ENVIRONMENT, TIMESTAMP)
 env = gym.make(ENVIRONMENT)
+env._max_episode_steps = 100000
 env = wrappers.Monitor(env, os.path.join(SUMMARY_DIR, ENVIRONMENT), force=True, video_callable=None)
-
-
-
-# Policy gradient has high variance, seed for reproducability
 env.seed(1)
-
-
 
 ## ENVIRONMENT Hyperparameters
 state_size = env.observation_space.shape[0]
-action_size = env.action_space.shape[0]
+action_size = env.action_space.n#shape[0]
 
 print ("[INFO] action_size: {}, state_size: {}".format(env.action_space.shape, state_size))
 
 
+######################################################################
+#                         Control Parameters                         # 
+######################################################################
+
+allRewards = []
+total_rewards = 0
+maximumRewardRecorded = 0
+episode = 0
+episode_states, episode_actions, episode_rewards = [],[],[]
+Train = True
 
 
-## TRAINING Hyperparameters
-max_episodes = 10000
-learning_rate = 0.01
-gamma = 0.95 # Discount rate
-
+######################################################################
+#                         Required Functions                         # 
+######################################################################
+def moving_average(a, n=3) :
+    ret = np.cumsum(a, dtype=float)
+    ret[n:] = ret[n:] - ret[:-n]
+    return ret[n - 1:] / n
 
 
 def discount_and_normalize_rewards(episode_rewards):
     discounted_episode_rewards = np.zeros_like(episode_rewards)
     cumulative = 0.0
+        cumulative = cumulative * GAMMA + episode_rewards[i]
     for i in reversed(range(len(episode_rewards))):
-        cumulative = cumulative * gamma + episode_rewards[i]
         discounted_episode_rewards[i] = cumulative
     
     mean = np.mean(discounted_episode_rewards)
@@ -61,6 +77,10 @@ def restore_model(model_path, saver, sess):
     saver.restore(sess, os.path.join(model_path, "model.ckpt"))
     print("Model restored from", model_path)
 
+
+######################################################################
+#                       Tensorflow Graph Define                      #
+######################################################################
 
 with tf.name_scope("inputs"):
     input_ = tf.placeholder(tf.float32, [None, state_size], name="input_")
@@ -101,43 +121,31 @@ with tf.name_scope("inputs"):
         
     
     with tf.name_scope("train"):
-        train_opt = tf.train.AdamOptimizer(learning_rate).minimize(loss)
+        train_opt = tf.train.AdamOptimizer(LEARNING_RATE).minimize(loss)
 
 
 
 
-
+######################################################################
+#                             Optimization                           # 
+######################################################################
 
 # Setup TensorBoard Writer
 writer = tf.summary.FileWriter(SUMMARY_DIR)
-
-## Losses
 tf.summary.scalar("Loss", loss)
-
-## Reward mean
 tf.summary.scalar("Reward_mean", mean_reward_)
-
 write_op = tf.summary.merge_all()
-
-
-
-allRewards = []
-total_rewards = 0
-maximumRewardRecorded = 0
-episode = 0
-episode_states, episode_actions, episode_rewards = [],[],[]
-
-
-
-
-
 saver = tf.train.Saver()
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
 
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    
-    for episode in range(max_episodes):
+if Train:
+    ##########################################################################
+    #                           Model Training                               #
+    ##########################################################################
+    for episode in range(NUM_EPISODES):
         
+        for tt in range(MAX_T):
         episode_rewards_sum = 0
 
         # Launch the game
@@ -145,7 +153,6 @@ with tf.Session() as sess:
         
         # env.render()
            
-        while True:
             
             # Choose action a, remember WE'RE NOT IN A DETERMINISTIC ENVIRONMENT, WE'RE OUTPUT PROBABILITIES.
             action_probability_distribution = sess.run(action_distribution, feed_dict={input_: state.reshape([1,state_size])})
@@ -155,6 +162,7 @@ with tf.Session() as sess:
             new_state, reward, done, info = env.step(action)
 
             # Store s, a, r
+            if done or (tt == (MAX_T - 1)):
             episode_states.append(state)
             action_ = np.zeros(action_size)
             action_[action] = 1
@@ -162,7 +170,6 @@ with tf.Session() as sess:
             episode_actions.append(action_)
             
             episode_rewards.append(reward)
-            if done:
                 episode_rewards_sum = np.sum(episode_rewards)
                 allRewards.append(episode_rewards_sum)
                 total_rewards = np.sum(allRewards)
@@ -179,40 +186,59 @@ with tf.Session() as sess:
                 
                 # Calculate discounted reward
                 discounted_episode_rewards = discount_and_normalize_rewards(episode_rewards)
-                # discounted_episode_rewards = episode_rewards
-                                
 
-
-
-
-                # Feedforward, gradient and backpropagation
                 loss_, _ = sess.run([loss, train_opt], feed_dict={input_: np.vstack(np.array(episode_states)),
                                                                  actions: np.vstack(np.array(episode_actions)),
                                                                  discounted_episode_rewards_: discounted_episode_rewards 
                                                                 })
-                
- 
-                                
-
-
-
-                # Write TF Summaries
                 summary = sess.run(write_op, feed_dict={input_: np.vstack(np.array(episode_states)),
-                                                                 actions: np.vstack(np.array(episode_actions)),
-                                                                 discounted_episode_rewards_: discounted_episode_rewards,
-                                                                    mean_reward_: mean_reward
+                                                                actions: np.vstack(np.array(episode_actions)),
+                                                                discounted_episode_rewards_: discounted_episode_rewards,
+                                                                mean_reward_: mean_reward
                                                                 })
-                
-               
                 writer.add_summary(summary, episode)
                 writer.flush()
-                
-                # Reset the transition stores
                 episode_states, episode_actions, episode_rewards = [],[],[]
                 
                 if episode % 100 == 0 and episode > 0:
                     path = save_model(SUMMARY_DIR, saver, sess)
                     print('Saved model at episode', episode, 'in', path)
-                break
-            
+
+                env.close()
+                break            
             state = new_state
+
+
+
+    episode_rewards = moving_average(episode_rewards, n = 30)
+    episode_rewards[0] = mean_reward[10]
+    plt.plot(episode_rewards)
+    plt.plot(mean_reward[10:])
+    plt.title('Monte Carlo Policy Gradient Reward Convergence')
+    plt.legend(['Episode reward with smoothening widow of n = 25', 'Mean episode reward'])
+    plt.ylabel('Reward')
+    plt.xlabel('Episodes')
+    plt.show()
+
+else:
+    ##########################################################################
+    #                         Model Evaluation                               #
+    ##########################################################################
+
+    saver.restore(session, os.path.join(SUMMARY_DIR, "model.ckpt"))
+    state = env.reset()
+
+    env.render()
+
+    episode_reward = 0
+    
+    while True:
+        state = np.expand_dims(state, axis=0)
+        action_probability_distribution = sess.run(action_distribution, feed_dict={input_: state.reshape([1,state_size])})
+        action = np.argmax(action_probability_distribution[0])
+        state, reward, done, info = env.step(action)
+        episode_reward = episode_reward + reward
+        if done: break
+
+    print ("[INFO] Final evaluation reward: {}".format(episode_reward))
+    env.close()